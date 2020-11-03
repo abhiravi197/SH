83b32b2ecd9ac1b35f41f0426aedf391de3f3346
Abhishek Ravi <raviabhishek445@gmail.com>
2020-11-03 17:36:39 +0530

    settings file changed

--- Shap1/Shap1/settings.py
+++ Shap1/Shap1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Shap1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
