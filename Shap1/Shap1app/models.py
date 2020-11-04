e24fe524e70b8cf92fb8626450d3df5c8762c893
Minu Thomas <minu@antonal.com>
2020-11-04 12:00:26 +0530

    model changed

--- Shap1/Shap1app/models.py
+++ Shap1/Shap1app/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class UserInfo(AbstractUser):
-    gender=models.CharField(max_length=10)
+    gender=models.CharField(max_length=10,blank=True)
+    dob=models.DateField(blank=True,null=True)
+    nationality=models.CharField(max_length=100,blank=True)
+    country=models.CharField(max_length=100,blank=True)
+    countrycode=models.IntegerField(null=True)
+    phoneno=models.IntegerField(null=True)
