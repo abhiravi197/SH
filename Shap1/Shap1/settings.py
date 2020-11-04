9f95915126252dd90abf5f014df3c9b9bc081726
abhiravi197 <abhishek@antonal.com>
2020-11-04 12:24:58 +0530

    templates

--- Shap1/Shap1/settings.py
+++ Shap1/Shap1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Shap1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates/'],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATICFILES_DIR=[STATIC_DIR,]
