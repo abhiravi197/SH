d1d07d98853ec63a5b40cafdd1b877dba9f18548
Abhishek Ravi <raviabhishek445@gmail.com>
2020-11-04 09:59:43 +0530

    abtrat user modifed

--- Shap1/Shap1/settings.py
+++ Shap1/Shap1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Shap1app',
 ]
 
 MIDDLEWARE = [
@@ -98,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTH_USER_MODEL='Shap1app.UserInfo'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
